52e28e961c2134c087c0a031ff0145262186e13d
Samet Ronaer <s.ronaer@hotmail.com>
2018-09-22 01:58:11 +0300

    scanner kullanimi

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!s32");
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        System.out.println("ikinci init");
+        sc.nextInt();
 
     }
 }
